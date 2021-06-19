ac4c942be2e223da417ac7eed8c1dbb405bdd932
safie <b021810023@student.utem.edu.my>
2021-06-20 01:47:33 +0800

    saya dh tuka coding dengan tambah flow dalam excel

--- Exercise 15 Mutex/main.c
+++ Exercise 15 Mutex/main.c
@@ -22,8 +22,8 @@ osThreadId T_x2;
 osThreadId T_x3;
 osThreadId T_x4;
 
-osMessageQId Q_LED;
-osMessageQDef (Q_LED,0x16,unsigned char);
+osMessageQId bufferdata_que;
+osMessageQDef (bufferdata_que,0x16,unsigned char);
 osEvent  result;
 
 osMutexId x_mutex;
@@ -42,25 +42,39 @@ const unsigned int N = 4;
 unsigned char buffer[N];
 unsigned int insertPtr = 0;
 unsigned int removePtr = 0;
+unsigned char bufferdata;
+unsigned char buffer0;
+unsigned char buffer1;
+unsigned char buffer2;
+unsigned char buffer3;
 
 void put(unsigned char an_item){
 	osSemaphoreWait(space_semaphore, osWaitForever);
 	osMutexWait(x_mutex, osWaitForever);
 	buffer[insertPtr] = an_item;
+	buffer0=buffer[0];
+	buffer1=buffer[1];
+	buffer2=buffer[2];
+	buffer3=buffer[3];
 	insertPtr = (insertPtr + 1) % N;
 	osMutexRelease(x_mutex);
 	osSemaphoreRelease(item_semaphore);
 }
 
 unsigned char get(){
-	unsigned int rr = 0xff;
+	unsigned int rr = 0;
 	osSemaphoreWait(item_semaphore, osWaitForever);
 	osMutexWait(x_mutex, osWaitForever);
-	rr = buffer[removePtr];
+	bufferdata = buffer[removePtr];
+	buffer[removePtr]=rr;
+	buffer0=buffer[0];
+	buffer1=buffer[1];
+	buffer2=buffer[2];
+	buffer3=buffer[3];
 	removePtr = (removePtr + 1) % N;
 	osMutexRelease(x_mutex);
 	osSemaphoreRelease(space_semaphore);
-	return rr;
+	return bufferdata;
 }
 
 int loopcount = 20;
@@ -68,7 +82,7 @@ int loopcount = 20;
 void x_Thread1 (void const *argument) 
 {
 	//producer
-	unsigned char item = 0x30;
+	unsigned char item = 0x41;
 	for(; i<loopcount; i++){
 		put(item++);
 	}
@@ -81,18 +95,18 @@ void x_Thread2 (void const *argument)
 	for(; j<loopcount; j++){
 		data = get();
 		//SendChar(data);
-		osMessagePut(Q_LED,data,osWaitForever);             //Place a value in the message queue
+		osMessagePut(bufferdata_que,data,osWaitForever);             //Place a value in the message queue
 	}
 }
 
 void x_Thread3 (void const *argument) 
 {
 	//consumer (waiter #2)
-	unsigned int c2data = 0x00;
+	unsigned int data2 = 0x00;
 	for(; k<loopcount; k++){
-		c2data = get();
-		//SendChar(c2data);
-		osMessagePut(Q_LED,c2data,osWaitForever);             //Place a value in the message queue
+		data2 = get();
+		//SendChar(data2);
+		osMessagePut(bufferdata_que,data2,osWaitForever);             //Place a value in the message queue
 	}
 }
 
@@ -100,7 +114,7 @@ void x_Thread4(void const *argument)
 {
 	//cashier
 	for(;;){
-		result = 	osMessageGet(Q_LED,osWaitForever);				//wait for a message to arrive
+		result = 	osMessageGet(bufferdata_que,osWaitForever);				//wait for a message to arrive
 		SendChar(result.value.v);
 	}
 }
@@ -113,14 +127,12 @@ int main (void)
 	space_semaphore = osSemaphoreCreate(osSemaphore(space_semaphore), N);
 	x_mutex = osMutexCreate(osMutex(x_mutex));	
 	
-	Q_LED = osMessageCreate(osMessageQ(Q_LED),NULL);					//create the message queue
+	bufferdata_que = osMessageCreate(osMessageQ(bufferdata_que),NULL);					//create the message queue
 	
 	T_x1 = osThreadCreate(osThread(x_Thread1), NULL);//producer
 	T_x2 = osThreadCreate(osThread(x_Thread2), NULL);//consumer
 	T_x3 = osThreadCreate(osThread(x_Thread3), NULL);//another consumer
 	T_x4 = osThreadCreate(osThread(x_Thread4), NULL);//casher
-	
  
 	osKernelStart ();                         // start thread execution 
 }
-
